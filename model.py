43703b7c478c91b31b56ea8c86fceafa31f2f633
Luis Martinez <luis.martinez.sat@gmail.com>
2023-12-01 09:50:30 -0600

    added docstring to functions

--- model.py
+++ model.py
@@ -22,3 +22,35 @@ def preprocess_titanic(df):
     
 
     return df
+
+
+
+
+
+
+
+
+
+def preprocess_telco(df):
+    '''
+    preprocess_telco will take in a pandas dataframe of our telco data, 
+    expected to already be a newly acquired and preped copy of the  telco 
+    data set (see documentation on acquire.py and prepare.py)
+    
+    output:
+    encoded, ML-ready version of our clean data, with 
+    object type columns encoded in the one-hot fashion. The dataframe is
+    now ready to be split into train, validate, and test.
+    returns: pandas DataFrame.
+    '''
+     
+    encoding_vars = []
+    # loop through the columns to fill encoded_vars with appropriate datatype field names
+    for col in df.columns:
+        if df[col].dtype == 'O':
+            encoding_vars.append(col)
+    # encode our list of columns, concatenate columns, and drop columns
+    df_encoded_cats = pd.get_dummies(df[encoding_vars], drop_first=True).astype(int)
+    df = pd.concat([df,df_encoded_cats],axis=1).drop(columns=encoding_vars)
+    return df
+
